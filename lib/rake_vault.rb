950f8a6c70c6543d34b6ad660e254dfb86f1c397
Toby <tobyclemson@gmail.com>
2022-07-04 12:52:34 +0100

    Initial commit.

---
+++ lib/rake_vault.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require 'rake_vault/version'
+
+module RakeVault
+end
---
+++ spec/rake_vault_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe RakeVault do
+  it 'has a version number' do
+    expect(RakeVault::VERSION).not_to be_nil
+  end
+end
