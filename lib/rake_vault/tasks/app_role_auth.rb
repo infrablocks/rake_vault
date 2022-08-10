4f392ac59d4e687618c431fe80bf633448be2845
Jakob Durstberger <jakob@durstberger.me>
2022-08-10 16:34:50 +0200

    Add support for role_id and secret_id to app_role auth.

--- lib/rake_vault/tasks/app_role_auth.rb
+++ lib/rake_vault/tasks/app_role_auth.rb
@@ -6,8 +6,27 @@ module RakeVault
   module Tasks
     class AppRoleAuth < RakeFactory::Task
       default_name :login
-      action do |_t|
-        puts 'done'
+      default_prerequisites(RakeFactory::DynamicValue.new do |t|
+        [t.ensure_task_name]
+      end)
+      default_description(RakeFactory::DynamicValue.new do |_task|
+        'Login with app role using vault'
+      end)
+      parameter :address
+      parameter :ensure_task_name, default: :'vault:ensure'
+      parameter :path, default: 'auth/approle/login'
+      parameter :role_id
+      parameter :secret_id
+
+      action do |task|
+        role_id = task.role_id ? "role_id=#{task.role_id}" : nil
+        secret_id = task.secret_id ? "secret_id=#{task.secret_id}" : nil
+
+        RubyVault.write(
+          address: task.address,
+          path: task.path,
+          data: [role_id, secret_id].compact
+        )
       end
     end
   end
--- spec/rake_vault/tasks/app_role_auth_spec.rb
+++ spec/rake_vault/tasks/app_role_auth_spec.rb
@@ -5,6 +5,12 @@ require 'spec_helper'
 describe RakeVault::Tasks::AppRoleAuth do
   include_context 'rake'
 
+  before do
+    namespace :vault do
+      task :ensure
+    end
+  end
+
   def define_task(opts = {}, &block)
     opts = { namespace: :app_role }.merge(opts)
 
@@ -18,5 +24,122 @@ describe RakeVault::Tasks::AppRoleAuth do
 
     expect(Rake.application)
       .to(have_task_defined('app_role:login'))
+  end
+
+  it 'gives the task a description' do
+    define_task
+
+    expect(Rake::Task['app_role:login'].full_comment)
+      .to(eq('Login with app role using vault'))
+  end
+
+  it 'allows multiple login tasks to be declared' do
+    define_task(namespace: :app_role1)
+    define_task(namespace: :app_role2)
+
+    expect(Rake.application).to(have_task_defined('app_role1:login'))
+    expect(Rake.application).to(have_task_defined('app_role2:login'))
+  end
+
+  it 'depends on the vault:ensure task by default' do
+    define_task
+
+    expect(Rake::Task['app_role:login'].prerequisite_tasks)
+      .to(include(Rake::Task['vault:ensure']))
+  end
+
+  it 'passes a path parameter of auth/approle/login to write by default' do
+    define_task
+    stub_ruby_vault
+
+    Rake::Task['app_role:login'].invoke
+
+    expect(RubyVault)
+      .to(have_received(:write)
+            .with(hash_including(path: 'auth/approle/login')))
+  end
+
+  it 'passes the provided value for the path parameter to write ' \
+     'when present' do
+    path = 'auth/differentapprole/login'
+    define_task do |t|
+      t.path = path
+    end
+    stub_ruby_vault
+
+    Rake::Task['app_role:login'].invoke
+
+    expect(RubyVault)
+      .to(have_received(:write)
+            .with(hash_including(path: path)))
+  end
+
+  it 'does not pass role_id to write within data parameter ' \
+     'when role_id is nil' do
+    define_task do |t|
+      t.role_id = nil
+    end
+    stub_ruby_vault
+
+    Rake::Task['app_role:login'].invoke
+
+    expect(RubyVault)
+      .to(have_received(:write)
+            .with(hash_including(
+                    data: excluding(a_string_including('role_id='))
+                  )))
+  end
+
+  it 'passes the provided role_id to write within data parameter ' \
+     'when present' do
+    define_task do |t|
+      t.role_id = 'some-role-id'
+    end
+    stub_ruby_vault
+
+    Rake::Task['app_role:login'].invoke
+
+    expect(RubyVault)
+      .to(have_received(:write)
+            .with(hash_including(
+                    data: including('role_id=some-role-id')
+                  )))
+  end
+
+  it 'does not pass secret_id to write within data parameter ' \
+     'when secret_id is nil' do
+    define_task do |t|
+      t.secret_id = nil
+    end
+    stub_ruby_vault
+
+    Rake::Task['app_role:login'].invoke
+
+    expect(RubyVault)
+      .to(have_received(:write)
+            .with(hash_including(
+                    data: excluding(a_string_including('secret_id='))
+                  )))
+  end
+
+  it 'passes the provided secret_id to write within data parameter ' \
+     'when present' do
+    define_task do |t|
+      t.secret_id = 'some-secret-id'
+    end
+    stub_ruby_vault
+
+    Rake::Task['app_role:login'].invoke
+
+    expect(RubyVault)
+      .to(have_received(:write)
+            .with(hash_including(
+                    data: including('secret_id=some-secret-id')
+                  )))
+  end
+
+  def stub_ruby_vault
+    allow(RubyVault).to(receive(:write))
+    allow(RubyVault).to(receive(:configure))
   end
 end
